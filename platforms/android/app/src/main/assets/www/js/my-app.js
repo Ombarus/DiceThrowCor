f84114706f61855541ceec88150fcfbc61b74bc0
matchoc <matchoc@gmail.com>
2018-09-01 14:28:10 +0900

    + Added all the basic pages - Basic flow, layout and look&feel pretty much working ! Starting on the Javascript implementation of events and data save/restore

--- platforms/android/app/src/main/assets/www/js/my-app.js
+++ platforms/android/app/src/main/assets/www/js/my-app.js
@@ -1,13 +1,17 @@
 var app = new Framework7({
 	root: '#app',
-    routes: [
-        {
-            path: "/login/",
-            url: document.location.pathname.split("index.html")[0] + "login.html"
-        }
-    ],
+    routes: routes,
     panel: {
         swipe: "left",
     },
 });
 var mainView = app.views.create('.view-main');
+var $$ = Dom7;
+$$(document).on('page:init', function (e) {
+    $$('#threshold-slider').on('touchstart', function(event) {
+		app.swiper.get($$('.tabs-swipeable-wrap')).allowTouchMove = false;
+	});
+	$$('#threshold-slider').on('touchend', function(event) {
+		app.swiper.get($$('.tabs-swipeable-wrap')).allowTouchMove = true;
+	});
+});
