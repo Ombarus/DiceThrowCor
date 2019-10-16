9b7eead5c624a3157dc48353c1a645a988687414
ombarus <ombarus.dev@gmail.com>
2019-10-16 21:27:24 +0900

    -Updated plugins, added plugin to port shortcuts to new androidx, v2.2.0

--- sign.bat
+++ sign.bat
@@ -1,15 +1,21 @@
 pushd %~dp0
 
-copy platforms\android\app\build\outputs\apk\release\app-release-unsigned.apk C:\Dev\Projects\GooglePlay
-cd C:\Dev\Projects\GooglePlay
+if "%2" == "" (
+	set CONFIG=release
+) else (
+	set CONFIG=%2
+)
+
+copy platforms\android\app\build\outputs\apk\%CONFIG%\app-%CONFIG%-unsigned.apk F:\Dev\Projects\GooglePlay
+cd F:\Dev\Projects\GooglePlay
 "C:\Program Files\Java\jdk1.8.0_181\bin\jarsigner.exe" -verbose -sigalg SHA1withRSA -digestalg SHA1 -keystore android.keystore app-release-unsigned.apk android-app-key
-"C:\Program Files (x86)\Android\android-sdk\build-tools\23.0.1\zipalign.exe" -v 4 app-release-unsigned.apk app-release-signed.apk
+"C:\Program Files (x86)\Android\android-sdk\build-tools\23.0.1\zipalign.exe" -v 4 app-%CONFIG%-unsigned.apk app-%CONFIG%-signed.apk
 mkdir "Free %1"
 
-copy app-release-signed.apk "Free %1"
-copy app-release-unsigned.apk "Free %1"
+copy app-%CONFIG%-signed.apk "Free %1"
+copy app-%CONFIG%-unsigned.apk "Free %1"
 
-del app-release-unsigned.apk
-del app-release-signed.apk
+del app-%CONFIG%-unsigned.apk
+del app-%CONFIG%-signed.apk
 
 popd
